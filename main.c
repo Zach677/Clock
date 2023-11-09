835c0c6a5278714fa7889d4565655bd962093690
Star <i@xingbest.xyz>
2023-11-09 14:19:25 +0800

    feat: add ms
    Signed-off-by: Star <i@xingbest.xyz>

--- main.c
+++ main.c
@@ -3,20 +3,20 @@
 
 int main()
 {
-  time_t start_time, end_time;
+  clock_t start_time, end_time;
   double elapsed_time;
 
   printf("按下Enter键开始计时");
   getchar(); // 等待用户按下enter
 
-  start_time = time(NULL);
+  start_time = clock();
 
   printf("计时器已启动,请按enter键停止...");
   getchar(); // 等待用户按下enter
 
-  end_time = time(NULL);
+  end_time = clock();
 
-  elapsed_time = difftime(end_time, start_time);
+  elapsed_time = (double)(end_time - start_time);
 
   printf("计时器已停止, 经过%.2f 秒\n", elapsed_time);
 
